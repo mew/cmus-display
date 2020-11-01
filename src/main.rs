584c3e38d39464174af1479b7ad7d3da49cf95a2
Nora <ncos@protonmail.ch>
2020-11-01 18:09:41 -0500

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let cmus_remote = String::from_utf8(Command::new("cmus-remote").arg("-Q").output().unwrap().stdout).unwrap();
+    let x: Vec<&str> = cmus_remote.split("\n").collect();
+    let mut artist = "";
+    let mut track = "";
+    for i in 0..x.len() {
+        let a = x[i];
+        if a.starts_with("tag ") {
+            if a.starts_with("tag artist") {
+                artist = a.split("artist ").collect::<Vec<&str>>()[1];
+            } else if a.starts_with("tag title") {
+                track = a.split("title ").collect::<Vec<&str>>()[1];
+            }
+        }
+    }
+
+    println!("{} - {}", artist, track)
 }
